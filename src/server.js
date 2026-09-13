0c65f5224fe0f8c1dd021f36a450d71934d399a5
karnaukhalina15-jpg <karnaukh.alina15@gmail.com>
2026-09-13 09:31:52 +0200

    add all hw2 node

--- src/server.js
+++ src/server.js
@@ -18,19 +18,6 @@ app.use(cors());
 app.use(logger);
 app.use(notesRouter);
 
-// app.get('/notes', (req, res) => {
-//   res.status(200).json({
-//     message: 'Retrieved all notes',
-//   });
-// });
-
-// app.get('/notes/:noteId', (req, res) => {
-//   const { noteId } = req.params;
-//   res.status(200).json({
-//     message: `Retrieved note with ID: ${noteId}`,
-//   });
-// });
-
 app.use(notFoundHandler);
 app.use(errorHandler);
 
